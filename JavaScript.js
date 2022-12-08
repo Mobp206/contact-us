38cd712ed844dc7ecd0ee2873b3400772885278c
Mobp206 <114334374+Mobp206@users.noreply.github.com>
2022-12-07 21:18:32 -0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,4 @@
-﻿function validateForm() {
+function validateForm() {
 
     var errorMessages = "";
     var Firstname = document.getElementById("FirstName").value;
@@ -14,15 +14,21 @@
     var userEmail = document.getElementById("Email").value;
     var atpos = userEmail.indexOf("@");
     var dotpos = userEmail.lastIndexOf(".");
-    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 <= userEmail.length) {
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
         errorMessages += "<p>Invalid email</p>";
-
+        console.log("email invalid")
+    } else {
+        validEmail = true;
+        console.log("Email valid")
     }
 
     var phone = document.getElementById("Phone").value;
     if (isNaN(phone) || phone.length > 15 || phone === null || phone === "") {
         errorMessages += "<p>Invalid phone number</p>";
 
+    } else {
+        validphone = true;
+        console.log("VALID PHONE #")
     }
 
     var Username = document.getElementById("Username").value;
@@ -51,9 +57,3 @@
 //Zipcode is OK. For example: validZipcode = true;
 
 }
-
-
-
-
-
-
